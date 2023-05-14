0f2c0906b19b07c16e5d78f26853876d55bff24a
GreenClothes <kybin.kim@gmail.com>
2023-05-14 21:12:27 +0900

    BAEKJOON 1992 ongoing

---
+++ DIVIDE_AND_CONQUER/1992.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/1992
+
+import sys
+input = sys.stdin.readline
+
+N = int(input().strip())
+Q = [input().strip() for _ in range(N)]
+ans = ['(']
+
+def DC(x, y, n):
+    img = Q[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if img != Q[i][j]:
+                ans.append('(')
+                DC(x, y, n // 2)
+                DC(x + n // 2, y, n // 2)
+                DC(x, y + n // 2, n // 2)
+                DC(x + n // 2, y + n // 2, n // 2)
+                ans.append(')')
+                return
+    ans.append(img)
+
+DC(0, 0, N)
+print(*ans)
