8f5e28991dd114a95c42fe62b2b6d46122973c32
GreenClothes <kybin.kim@gmail.com>
2023-06-14 10:37:13 +0900

    BAEKJOON 1780

--- DIVIDE_AND_CONQUER/1780.py
+++ DIVIDE_AND_CONQUER/1780.py
@@ -5,4 +5,24 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 P = [list(map(int, input().strip().split())) for _ in range(N)]
+num_paper = []
 
+def DC(x, y, n):
+    p = P[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if p != P[i][j]:
+                DC(x, y, n//3)
+                DC(x+n//3, y, n//3)
+                DC(x, y+n//3, n//3)
+                DC(x+n//3, y+n//3, n//3)
+                DC(x+n*2//3, y, n//3)
+                DC(x, y+n*2//3, n//3)
+                DC(x+n*2//3, y+n*2//3, n//3)
+                DC(x+n*2//3, y+n//3, n//3)
+                DC(x+n//3, y+n*2//3, n//3)
+                return
+    num_paper.append(p)
+
+DC(0, 0, N)
+print(num_paper.count(-1), num_paper.count(0), num_paper.count(1), sep='\n')
