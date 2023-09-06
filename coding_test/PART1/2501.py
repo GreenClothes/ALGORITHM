2aacc84f794b02bfe28467be7797b2a611e56215
GreenClothes <kybin.kim@gmail.com>
2023-09-06 10:32:44 +0900

    BAEKJOON 2501

---
+++ coding_test/PART1/2501.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2501
+
+N, K = map(int, input().split())
+measure_dict = dict()
+for i in range(1, N//2):
+    if i in measure_dict:
+        break
+    if N%i == 0:
+        measure_dict[i] = True
+        measure_dict[N//i] = True
+
+measures = sorted(measure_dict.keys())
+if len(measures) < K:
+    print(0)
+else:
+    print(measures[K-1])
