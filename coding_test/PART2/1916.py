df0514d49a88e1e47cf91542ef1bba17c60864c6
GreenClothes <kybin.kim@gmail.com>
2023-09-27 13:53:09 +0900

    BAEKJOON 1916 solved

--- coding_test/PART2/1916.py
+++ coding_test/PART2/1916.py
@@ -1,32 +1,33 @@
 # https://www.acmicpc.net/problem/1916
 
 import heapq
+from sys import maxsize
 
 N = int(input())
 M = int(input())
-BUS = {i:dict() for i in range(N+1)}
+BUS = [[] for _ in range(N+1)]
 for _ in range(M):
     a, b, c = map(int, input().split())
-    BUS[a][b] = c
+    BUS[a].append((b, c))
 start, end = map(int, input().split())
-visited = {i:100000 for i in range(N+1)}
+visited = {i:maxsize for i in range(N+1)}
 
 def dijkstra(s):
     q = []
-    heapq.heappush(q, (s, 0))
+    heapq.heappush(q, (0, s))
     visited[s] = 0
 
     while q:
-        bus, cost = heapq.heappop(q)
+        cost, bus = heapq.heappop(q)
 
         if visited[bus] < cost:
             continue
 
-        for next_bus, next_cost in BUS[bus].items():
+        for next_bus, next_cost in BUS[bus]:
             expect_cost = cost + next_cost
 
             if visited[next_bus] > expect_cost:
-                heapq.heappush(q, (next_bus, expect_cost))
+                heapq.heappush(q, (expect_cost, next_bus))
                 visited[next_bus] = expect_cost
 
 dijkstra(start)
